51cb1b8815ed799da6a97c7646a3ac64e1af999d
Gabriel Paul "Cley Faye" Risterucci <cleyfaye@gmail.com>
2019-08-14 21:34:42 +0200

    Removed local files from git

--- updatelocal.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd ..\boilerplate
-call npm pack
-cd ..\storyteller
-call npm install -D ..\boilerplate\cley_faye-boilerplate-0.1.1.tgz
-powershell -command "Get-ChildItem node_modules -Include .cache -Recurse | Remove-Item -Recurse"
