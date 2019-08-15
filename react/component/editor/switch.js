760b80d12c12bd5e7c8ab8464ad2053f7532c4dd
Gabriel Paul "Cley Faye" Risterucci <cleyfaye@gmail.com>
2019-08-15 02:10:19 +0200

    Unknown route redirect

--- react/component/editor/switch.js
+++ react/component/editor/switch.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Switch} from "react-router-dom";
 import {Route} from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import Welcome from "./welcome";
 
 class EditorSwitch extends React.Component {
   render() {
     return <Switch>
       <Route path="/editor" component={Welcome} />
+      <Redirect to="/editor" />
     </Switch>;
   }
 }
