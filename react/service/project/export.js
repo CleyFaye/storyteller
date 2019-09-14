ad338534cf2add2b39bf1c253493707deff185a3
Gabriel Paul "Cley Faye" Risterucci <cleyfaye@gmail.com>
2019-09-14 23:45:55 +0200

    Fixed missing info on export file

--- react/service/project/export.js
+++ react/service/project/export.js
@@ -4,6 +4,7 @@
  */
 export const exportProject = projectCtx => new Promise(resolve => {
   const result = {};
+  result.magicVersion = 2;
   result.title = projectCtx.title;
   result.parts = projectCtx.parts.map(
     (_, id) => projectCtx.exportPart(id)
